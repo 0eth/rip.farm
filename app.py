f8ab0530ccfc34b583399e05b70cc26b404750f2
0eth <57970534+0eth@users.noreply.github.com>
2025-07-16 21:10:04 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -51,12 +51,12 @@ def has_allowed_extension(url):
 def extract_assets_with_selenium(url):
     try:
         chrome_options = Options()
-        chrome_options.binary_location = "/usr/bin/chromium"
+        chrome_options.binary_location = "./drivers/chromedriver"
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
 
-        service = Service("/usr/bin/chromedriver")
+        service = Service("./drivers/chromedriver")
         driver = webdriver.Chrome(service=service, options=chrome_options)
 
         print(f"[DEBUG] Loading URL: {url}")
